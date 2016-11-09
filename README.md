abe002d021ce6a411a1a9f648f663281ef5b219b
Nir Oren <niroren@Nirs-MacBook-Pro-2.local>
2016-11-09 07:22:23 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,89 @@
+# Getting Started
+The clients directory contains a sample confederate (identical to an AI), and a sample judge. The controller directory has a html file which can be used to run the competition, while the server directory has the actual server, written in javascript. The html files utilise jquery and the socket-io client, while the server program is built on top of express and also uses the socket-io library.
+
+## Configuration
+Edit the names and secrets in the `server.js` file, and the names, secrets and urls in the HTML files.
+
+## Running
+To run, install node, and run `node server.js`. You should then be able to open the html files in your browser.
+
+# Message Types
+Below, we break down messages by topic. An AI client will send messages to the server on the following topics:
+1. __control__
+2. __message__
+
+The client will receive messages on the following topics:
+1. __AuthError__
+2. __roundInformation__
+3. __recap__
+4. __message__
+5. __TargetError__
+
+## Sending Messages
+
+The messages sent and received by a client are JSON strings, and more precisely, JSON maps (hashes). All messages _from a client_ contain authentication information within the JSON map. This authentication information uses the pair
+```javascript
+"id": <client ID>,
+"secret": <a secret>
+```
+The _client ID_ and _secret_ will be given to the client at the start of the competition and are used to uniquely identify it. In addition, the client will be provided with the server's URL.
+
+### Control messages
+A control message sent by a client has one more entry in the map, namely a `"status"` entry, which can take on the following values
+1. `register`
+2. `roundInformation`
+3. `recap`
+4. `endRound`
+5. `newRound`
+6. `startRound`
+
+The `register` message is used when connecting, to inform the server that the client is present. The server will not provide a response to such a message.
+
+The `roundInformation` message requests information about the round status, the round number, and the partners assigned to the client in this round. The server will thus respond to this message with a message on the __roundInformation__ topic of the form
+```javascript
+"roundNumber":<an integer between 0 and 3 (inclusive)>,
+"status":<one of "Running"|"Not Started"|"Finished">,
+"partners":["<a string representing the name of the partner the client should be talking to"]
+```
+
+___Note___: rounds run from 0 to 3. Before the competition starts, roundNumber will be -1.
+
+Example:
+```
+{"roundNumber":3,"status":"Running","partners":["judge3"]}
+```
+
+The `recap` message will send all messages sent so far in the round to the client, on the __recap__ topic. These form an array of messages (see the next section).
+
+The `endRound`,`startRound` and `newRound` messages are all sent on the __control__ topic. These messages are not sent in response to a message from the client, but rather inform the client of a change in round status.
+
+The `endRound` message is used to signal the fact that communication for the round is finished (i.e., judges and clients must stop communicating). The `startRound` message on the other hand tells judges and clients that they may start talking to each other. The `newRound` message tells clients that a new round is about to begin. This message also has another element in its map, namely the partner the client should talk to during this round, stored in a map with the name `"partners"`. The key for this map is the client name, while the value is an array of partners.
+
+Example:
+```javascript
+{"status":"newRound","partners": {"judge0":["conf0","ai0"],"judge1":["conf1","ai1"],"judge2":["conf2","ai2"],"judge3":["conf3","ai3"],"ai0":["judge0"],"ai1":["judge1"],"ai2":["judge2"],"ai3":["judge3"],"conf0":["judge0"],"conf1":["judge1"],"conf2":["judge2"],"conf3":["judge3"]}}
+```
+
+
+### Communication messages
+Standard messages are sent along the __message__ topic. Apart from the validation information, they contain two more entries: a `"to":<partner name>` entry - identifying who the message is for; and a `"content":<string>` containing the actual message.
+Example:
+```javascript
+{"id":"ai0","secret":"slkgj!sg1","content":"hello world","to":"judge0"}
+```
+
+The client will also receive communication messages on the __message__ topic, with the same format, but with the `secret` stripped out.
+
+### Errors
+Messages along the __AuthError__ topic indicate an authentication error, and contain a string saying "Invalid Secret".
+
+Messages on the __TargetError__ topic indicate that the client attempted to send a message to someone they're not supposed to be in communication with, or during the wrong phase of the competition (e.g., before the current round was Running).
+
+# Protocol
+The client should connect to the server's URL using websockets, and immediately send a `register` message.
+
+They can then send control messages at any time to the server (e.g., if they get disconnected and wish to determine what the current round status is on reconnecting).
+
+The server can also send control messages to the client at any time (see above as to which control messages the server may send).
+
+Communication messages should only be sent when the round is Running, doing so at other stages will result in an error being sent to the client.
---
+++ server/server.js
@@ -0,0 +1,318 @@
+var app=require('express')();
+var http=require('http').Server(app);
+var io=require('socket.io')(http);
+var client=require('socket.io-client');
+var fs=require('fs');
+io.set('origins','*:*');
+
+config={};
+
+config.judge=[];
+config.ai=[];
+config.confederate=[];
+
+config.judge[0]="judge0";
+config.judge[1]="judge1";
+config.judge[2]="judge2";
+config.judge[3]="judge3";
+
+config.ai[0]="ai0";
+config.ai[1]="ai1";
+config.ai[2]="ai2";
+config.ai[3]="ai3";
+config.confederate[0]="conf0";
+config.confederate[1]="conf1";
+config.confederate[2]="conf2";
+config.confederate[3]="conf3";
+
+config.secret={};
+config.secret["CONTROLLER"]="abv123";
+
+config.secret["judge0"]="alice";
+config.secret["judge1"]="alice";
+config.secret["judge2"]="alice";
+config.secret["judge3"]="alice";
+config.secret["conf0"]="abc123";
+config.secret["conf1"]="abc123";
+config.secret["conf2"]="abc123";
+config.secret["conf3"]="abc123";
+config.secret["ai0"]="abc123";
+config.secret["ai1"]="abc123";
+config.secret["ai2"]="abc123";
+config.secret["ai3"]="abc123";
+
+//each round stores the confederate
+//number and ai number for judge 0,0,1,2 respectively
+config.rounds=[]
+config.rounds[0]=[[0,1,2,3],[0,1,2,3]];
+config.rounds[1]=[[3,0,1,2],[1,2,3,0]];
+config.rounds[2]=[[2,3,0,1],[3,2,1,0]];
+config.rounds[3]=[[1,2,3,0],[2,1,0,3]];
+
+function getJudge(name)
+{
+  return config.judge.indexOf(name);
+}
+function getAI(name)
+{
+  return config.ai.indexOf(name);
+}
+function getConfederate(name)
+{
+  return config.confederate.indexOf(name);
+}
+
+function isJudge(name)
+{
+  return getJudge(name)!=-1;
+}
+
+function isAI(name)
+{
+  return getAI(name)!=-1;
+}
+
+function isConfederate(name)
+{
+  return getConfederate(name)!=-1;
+}
+
+function getConfederateForJudge(judgeNumber,round)
+{
+  return config.rounds[round][0][judgeNumber];
+}
+
+function getAIForJudge(judgeNumber,round)
+{
+  return config.rounds[round][1][judgeNumber];
+}
+
+function getJudgeForConfederate(confederateNumber,round)
+{
+  return config.rounds[round][0].indexOf(confederateNumber);
+}
+
+function getJudgeForAI(aiNumber,round)
+{
+  return config.rounds[round][1].indexOf(aiNumber);
+}
+
+function getConfederateForJudgeByName(judgeNumber,round)
+{ //TODO: catch appropriate exception if judgeNumber or round is incorrect
+  return config.confederate[getConfederateForJudge(judgeNumber,round)];
+}
+
+function getJudgeForConfederateByName(judgeNumber,round)
+{ //TODO: catch appropriate exception if judgeNumber or round is incorrect
+  return config.judge[getJudgeForConfederate(judgeNumber,round)];
+}
+
+function getAIForJudgeByName(judgeNumber,round)
+{ //TODO: catch appropriate exception if judgeNumber or round is incorrect
+  return config.ai[getAIForJudge(judgeNumber,round)];
+}
+
+function getJudgeForAIByName(judgeNumber,round)
+{
+  return config.judge[getJudgeForAI(judgeNumber,round)]
+}
+
+RUNNING="Running";
+NOTSTARTED="Not Started";
+FINISHED="Finished";
+currentRound=-1;
+clients={};
+roundStatus=FINISHED;
+//scores: for each round, a judge decides which partner is human and which ai.
+//So we have a hash of judgename->[confederate,mark] in each round.
+//The mark is used at the end as the score given to teh confederate
+//for "humanness".
+scores=[{},{},{},{}];
+//messages is an array of maps storing all messages sent to/from the client.
+//In turn, each map is a client -> messages (array) entry storing the ordered
+//list of messages sent to/from the client.
+messages=[{},{},{},{}];
+//////////////////////////////////////////////////////////////////////////
+function validate(socket,data)
+{
+  o=JSON.parse(data);
+  if (config.secret[o["id"]]==o["secret"])
+    return true;
+  socket.emit("AuthError","Invalid secret");
+  return false;
+}
+
+function handleDisconnect(socket,data)
+{
+  var toRemove;
+  for (var c in clients){
+    if (clients[c]==socket)
+    {
+      toRemove=c;
+      break;
+    }
+  }
+  delete clients[toRemove];
+}
+
+//messages from the controller
+function handleControllerMessage(socket,data)
+{
+  if (!validate(socket,data))
+    return;
+  o=JSON.parse(data);
+  switch(o["status"]){
+    case "statusUpdate": emitStatusUpdate(socket);
+                         break;
+    case "endRound": broadcast('control','{"status":"endRound"}');
+                     roundStatus=FINISHED;
+                     break;
+    case "newRound": currentRound+=1;
+                     var partners=JSON.stringify(informPartners());
+                     broadcast('control','{"status":"newRound","partners": '+partners+'}');
+                     roundStatus=NOTSTARTED;
+                     break;
+    case "startRound": broadcast('control','{"status":"startRound"}');
+                       roundStatus=RUNNING;
+                       break;
+  }
+}
+
+//control messages from the clients
+function handleControlMessage(socket,data)
+{
+  if (!validate(socket,data))
+    return;
+  o=JSON.parse(data);
+  switch(o["status"]){
+    case "register": clients[o["id"]]=socket;break;
+    case "roundInformation":var i=JSON.stringify(informPartners());
+                            socket.emit('roundInformation','{"roundNumber":'+roundNumber+', "status":'+roundStatus+', "partners":'+i+'}');
+                            break;
+    case "recap": var c=o.id;
+                  socket.emit('recap',JSON.stringify(messages[currentRound][c]));
+                  break;
+
+  }
+}
+
+
+function handleCommunicationMessage(socket,data)
+{
+  if (!validate(socket,data))
+    return;
+  o=JSON.parse(data);
+  c=o.id;
+  if (roundStatus==RUNNING &&
+      isAI(c) && o.to==getJudgeForAIByName(getAI(c),currentRound) ||
+      isConfederate(c) && o.to==getJudgeForConfederateByName(getConfederate(c),currentRound) ||
+      isJudge(c) && (o.to==getConfederateForJudgeByName(getJudge(c),currentRound) ||
+                     o.to==getAIForJudgeByName(getJudge(c),currentRound)) &&
+      clients[o.to]!=undefined
+     )
+  {
+    o.secret="";
+    //initialise the message array as needed.
+    if (messages[currentRound][o.id]==undefined)
+      messages[currentRound][o.id]=[];
+    if (messages[currentRound][o.to]==undefined)
+        messages[currentRound][o.to]=[];
+
+    messages[currentRound][o.id].push(o);
+    messages[currentRound][o.to].push(o);
+    clients[o.to].emit("message",JSON.stringify(o));
+    //TODO: serialise the messages to disk for future playback
+  }
+  else
+  {
+    clients[c].emit("TargetError",'{"status":"Wrong Client or not connected"}');
+  }
+}
+
+function handleScoreMessage(socket,data)
+{
+  console.log(data);
+  if (!validate(socket,data))
+    return;
+  var o=JSON.parse(data);
+  var c=o.id;
+  scores[currentRound][c]=o.robot;
+}
+
+
+
+
+//computes who needs to talk to who in this round.
+function informPartners()
+{
+  p={}
+  if (currentRound<0)
+    return p;
+
+  for (var i=0;i<config.judge.length;i++)
+    p[config.judge[i]]=[getConfederateForJudgeByName(i,currentRound),
+                        getAIForJudgeByName(i,currentRound)];
+
+  for (var i=0;i<config.ai.length;i++)
+    p[config.ai[i]]=[getJudgeForAIByName(i,currentRound)];
+
+  for (var i=0;i<config.confederate.length;i++)
+    p[config.confederate[i]]=[getJudgeForConfederateByName(i,currentRound)];
+  return p;
+}
+
+//send something to everyone
+function broadcast(msgType,data)
+{
+  for (c in clients)
+    clients[c].emit(msgType,data);
+}
+
+//used to inform the controller as to what's going on.
+function emitStatusUpdate(socket)
+{
+  var response={};
+  response["roundNumber"]=currentRound;
+  response["roundStatus"]=roundStatus;
+  //create connected clients
+  var cc=[{},{},{}];
+  for (var i=0;i<config.judge.length;i++)
+  {
+      c=config.judge[i]
+      cc[0][c]=(c in clients);
+  }
+  for (var i=0;i<config.ai.length;i++)
+  {
+      c=config.ai[i]
+      cc[1][c]=(c in clients);
+  }
+  for (var i=0;i<config.confederate.length;i++)
+  {
+      c=config.confederate[i]
+      cc[2][c]=(c in clients);
+  }
+  response["connectedClients"]=cc;
+  var sr=[];
+  for (var i=0;i<config.judge.length;i++)
+  {
+    var j= config.judge[i];
+      if (currentRound>-1 && scores[currentRound][j]!=undefined)
+        sr.push(j)
+  }
+  response["scoredClients"]=sr;
+  response["scores"]=scores;
+
+  socket.emit('statusUpdate',JSON.stringify(response));
+}
+
+//launch the Server
+http.listen(8080,function(){console.log('listening');});
+//TODO: add serving of jquery and webcast
+io.on('connection',function(socket){
+  console.log('connection detected');
+  socket.on('disconnect',function(data){handleDisconnect(socket,data);});
+  socket.on('controller',function(data){handleControllerMessage(socket,data);});
+  socket.on('control',function(data){handleControlMessage(socket,data);});
+  socket.on('score',function(data){handleScoreMessage(socket,data);});
+  socket.on('message',function(data){handleCommunicationMessage(socket,data);});
+});
